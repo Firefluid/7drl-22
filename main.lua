cfa0c52e3351434b2157494e26fda220b9c147bc
Dimitri Erlikh <30989132+Firefluid@users.noreply.github.com>
2022-03-05 15:22:48 +0100

    Add basic chess pieces

--- main.lua
+++ main.lua
@@ -6,7 +6,24 @@ tileset = {
   },
   floor1 = {0, 0, 16, 16},
   floor2 = {16, 0, 16, 16},
-  pawn = {32, 0, 16, 32}
+  pawn_white = {32, 0, 16, 32},
+  pawn_black = {128, 0, 16, 32},
+  pawn_outline = {32, 32, 16, 32},
+  rook_white = {32 + 16, 0, 16, 32},
+  rook_black = {128 + 16, 0, 16, 32},
+  rook_outline = {32 + 16, 32, 16, 32},
+  bishop_white = {32 + 16 * 2, 0, 16, 32},
+  bishop_black = {128 + 16 * 2, 0, 16, 32},
+  bishop_outline = {32 + 16 * 2, 32, 16, 32},
+  knight_white = {32 + 16 * 3, 0, 16, 32},
+  knight_black = {128 + 16 * 3, 0, 16, 32},
+  knight_outline = {32 + 16 * 3, 32, 16, 32},
+  queen_white = {32 + 16 * 4, 0, 16, 32},
+  queen_black = {128 + 16 * 4, 0, 16, 32},
+  queen_outline = {32 + 16 * 4, 32, 16, 32},
+  king_white = {32 + 16 * 5, 0, 16, 32},
+  king_black = {128 + 16 * 5, 0, 16, 32},
+  king_outline = {32 + 16 * 5, 32, 16, 32}
 }
 
 function drawTile(tile, x, y)
@@ -17,6 +34,21 @@ function drawTile(tile, x, y)
       x , y)
 end
 
+function drawPiece(piece, team, x, y)
+  drawTile(piece .. "_" .. team, x, y)
+
+  -- Draw outline
+  if team == "white" then
+    love.graphics.setColor(0, 0, 0)
+  else
+    love.graphics.setColor(1, 0, 0)
+  end
+
+  drawTile(piece .. "_outline", x, y - 1)
+
+  love.graphics.setColor(1, 1, 1)
+end
+
 function love.load()
   tileset.meta.image = love.graphics.newImage(tileset.meta.file)
   tileset.meta.image:setFilter("nearest", "nearest")
@@ -24,7 +56,7 @@ end
 
 function love.draw()
   local transform = love.math.newTransform()
-  transform:scale(2)
+  transform:scale(4)
   love.graphics.applyTransform(transform)
 
   for y=1,8 do
@@ -37,9 +69,35 @@ function love.draw()
     end
   end
 
-  drawTile("pawn", 32, 32 - 16)
+  drawPiece("rook", "white", 1 * 16, 0 * 16)
+  drawPiece("rook", "white", 8 * 16, 0 * 16)
 
-  love.graphics.origin()
+  drawPiece("bishop", "white", 2 * 16, 0 * 16)
+  drawPiece("bishop", "white", 7 * 16, 0 * 16)
 
-  love.graphics.print("Hello World", 0, 0)
+  drawPiece("knight", "white", 3 * 16, 0 * 16)
+  drawPiece("knight", "white", 6 * 16, 0 * 16)
+
+  drawPiece("queen", "white", 4 * 16, 0 * 16)
+  drawPiece("king", "white", 5 * 16, 0 * 16)
+
+  for i=1,8 do
+    drawPiece("pawn", "white", i * 16, 1 * 16)
+  end
+
+  for i=1,8 do
+    drawPiece("pawn", "black", i * 16, 6 * 16)
+  end
+
+  drawPiece("rook", "black", 1 * 16, 7 * 16)
+  drawPiece("rook", "black", 8 * 16, 7 * 16)
+
+  drawPiece("bishop", "black", 2 * 16, 7 * 16)
+  drawPiece("bishop", "black", 7 * 16, 7 * 16)
+
+  drawPiece("knight", "black", 3 * 16, 7 * 16)
+  drawPiece("knight", "black", 6 * 16, 7 * 16)
+
+  drawPiece("queen", "black", 4 * 16, 7 * 16)
+  drawPiece("king", "black", 5 * 16, 7 * 16)
 end
